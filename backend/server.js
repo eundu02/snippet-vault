a312b99c453af5a42837db8ce795e59b33f3a913
eundu02 <ses01036457@gmail.com>
2026-03-21 18:00:29 +0900

    feature: snippets API 및 SQLite 초기 구조 추가

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const snippetRoutes = require("./routes/snippets");
+
 const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Snippet Vault Backend");
+  res.send("Snippet-Vault Backend Server");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.use("/snippets", snippetRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
