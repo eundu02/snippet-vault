00058d862c8bbbc1e1394e11b52cacb3bd2655d4
eundu02 <ses01036457@gmail.com>
2026-03-29 18:00:33 +0900

    refactor snippets api with mvc and search filter

---
+++ backend/services/snippetsService.js
@@ -0,0 +1,52 @@
+const snippetsModel = require("../models/snippetsModel");
+
+const getAllSnippets = async ({ keyword, language_id }) => {
+  return await snippetsModel.getAllSnippets({ keyword, language_id });
+};
+
+const getSnippetById = async (id) => {
+  return await snippetsModel.getSnippetById(id);
+};
+
+const createSnippet = async ({ title, code, description, language_id }) => {
+  return await snippetsModel.createSnippet({
+    title,
+    code,
+    description,
+    language_id,
+  });
+};
+
+const updateSnippet = async ({ id, title, code, description, language_id }) => {
+  const existingSnippet = await snippetsModel.findSnippetById(id);
+
+  if (!existingSnippet) {
+    return null;
+  }
+
+  return await snippetsModel.updateSnippet({
+    id,
+    title,
+    code,
+    description,
+    language_id,
+  });
+};
+
+const deleteSnippet = async (id) => {
+  const existingSnippet = await snippetsModel.findSnippetById(id);
+
+  if (!existingSnippet) {
+    return null;
+  }
+
+  return await snippetsModel.deleteSnippet(id);
+};
+
+module.exports = {
+  getAllSnippets,
+  getSnippetById,
+  createSnippet,
+  updateSnippet,
+  deleteSnippet,
+};
